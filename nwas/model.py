0c60f90dc87aa97c82299afa37194df6a032eede
Abhishek Sarkar <aksarkar@alum.mit.edu>
2017-09-12 15:00:47 -0500

    Fix inheritance hierarchy to avoid error in ed.util.copy
    According to http://edwardlib.org/api/model-development, new distributions
    should multiply inherit from ed.models.RandomVariable and
    tf.contrib.distributions.Distribution.
    
    However, implementing it like this causes problems in ed.util.copy because
    kwargs for Distribution end up getting passed into the subclass __init__

--- nwas/model.py
+++ nwas/model.py
@@ -10,9 +10,12 @@ from tensorflow.contrib.distributions import (
     RegisterKL,
     kl_divergence,
 )
-from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import (
+    array_ops,
+    check_ops,
+)
 
-class SpikeSlab(RandomVariable, Distribution):
+class distribution_SpikeSlab(Distribution):
     """Spike-and-slab prior (point-normal mixture; Mitchell & Beauchamp 1988).
 
     This is a compound distribution of indicator variables z and values theta.
@@ -22,31 +25,40 @@ class SpikeSlab(RandomVariable, Distribution):
 
     We want to use the variational approximation to efficiently estimate the
     posterior w.r.t. this prior. The conjugate mean-field variational
-    approximation admits an analytical KL (Carbonetto & Stephens, Bayesian Anal
-    2012).
+    approximation admits an analytical KL (Carbonetto & Stephens 2012).
 
     q(theta_j, z_j | alpha, beta, gamma) = alpha_j N(theta_j; beta_j, gamma_j^{-1}) + 
                                            (1 - \alpha_j) delta(\theta_j)
 
-    This distribution does not support sampling (does not scale to high
+    This implementation does not support sampling (does not scale to high
     dimensions). Instead, use the local reparameterization trick (see
-    nwas.model.GeneticValue; Kingma, Salimans, & Welling arXiv 2015).
+    nwas.model.GeneticValue; Kingma, Salimans, & Welling 2015).
 
     """
     def __init__(self, alpha, beta, gamma, validate_args=False,
                  allow_nan_stats=True, name='SpikeSlab'):
-        self._alpha = array_ops.identity(alpha, name="alpha")
-        self._beta = array_ops.identity(beta, name="beta")
-        self._gamma = array_ops.identity(gamma, name="gamma")
+        # This is needed so that tf.python.ops.distributions.Distribution saves
+        # positional arguments. Otherwise, ed.util.random_variables.copy
+        # blows up
+        parameters = locals()
 
-        super(SpikeSlab, self).__init__(
+        # c.f. ed.models.empirical
+        with tf.name_scope(name, values=[alpha, beta, gamma]):
+            with tf.control_dependencies([]):
+                self._alpha = array_ops.identity(alpha, name="alpha")
+                self._beta = array_ops.identity(beta, name="beta")
+                self._gamma = array_ops.identity(gamma, name="gamma")
+                check_ops.assert_same_float_dtype([self._alpha, self._beta, self._gamma])
+
+        # c.f. tf.python.ops.distributions.normal.Normal.__init__
+        super(distribution_SpikeSlab, self).__init__(
             allow_nan_stats=allow_nan_stats,
             dtype=self._beta.dtype,
             graph_parents=[self._alpha, self._beta, self._gamma],
             name=name,
+            parameters=parameters,
             reparameterization_type=FULLY_REPARAMETERIZED,
             validate_args=validate_args,
-            value=tf.zeros_like(self._beta),
         )
 
     def _log_prob(self, value):
@@ -65,13 +77,12 @@ class SpikeSlab(RandomVariable, Distribution):
         return (self._alpha / self._gamma +
                 self._alpha * (1 - self._alpha) * tf.square(self._beta))
 
-@RegisterKL(SpikeSlab, SpikeSlab)
+@RegisterKL(distribution_SpikeSlab, distribution_SpikeSlab)
 def kl_spikeslab(q, p, name=None):
     """KL divergence between point-normal mixture and conjugate mean-field variational approximation
 
-    An analytic form was given without derivation in Carbonetto & Stephens,
-    Bayesian Anal 2012. It can be derived using Rasmussen and Williams, 2006,
-    Eqs. A.22, A.23
+    An analytic form was given without derivation in Carbonetto & Stephens
+    2012. It can be derived using Rasmussen and Williams 2006, Eqs. A.22, A.23
 
     """
     kl_qtheta_ptheta = q._alpha * kl_divergence(
@@ -84,7 +95,28 @@ def kl_spikeslab(q, p, name=None):
     )
     return tf.reduce_sum(kl_qtheta_ptheta + kl_qz_pz)
 
-class GeneticValue(RandomVariable, Distribution):
+class SpikeSlab(RandomVariable, distribution_SpikeSlab):
+    def __init__(self, *args, **kwargs):
+        if 'beta' in kwargs:
+            beta = kwargs['beta']
+        else:
+            beta = args[1]
+        if 'value' not in kwargs:
+            kwargs['value'] = tf.zeros_like(beta)
+        RandomVariable.__init__(self, *args, **kwargs)
+
+class distribution_GeneticValue(Distribution):
+    """Represent the local reparamterization eta = x * theta as proposed in Kingma, Salimans, & Welling 2015.
+
+    We use this to avoid sampling from the spike-and-slab prior. We have:
+
+    E_q[x theta] = x * (alpha .* beta)
+    V_q[x theta] = x^2 * (alpha ./ gamma + alpha .* (1 - alpha) .* beta^2)
+
+    Then, we can just sample from a Gaussian and scale appropriately to
+    evaluate E_q[ln p(x | ...)].
+
+    """
     def __init__(self, x, theta, validate_args=False, allow_nan_stats=True,
                  name='GeneticValue'):
         self._x = x
@@ -96,7 +128,7 @@ class GeneticValue(RandomVariable, Distribution):
                                     self._theta.variance()))
         )
 
-        super(GeneticValue, self).__init__(
+        super(distribution_GeneticValue, self).__init__(
             allow_nan_stats=allow_nan_stats,
             dtype=self._theta.dtype,
             name=name,
@@ -112,3 +144,7 @@ class GeneticValue(RandomVariable, Distribution):
 
     def _sample_n(self, n, seed=None):
         return self._dist._sample_n(n, seed)
+
+class GeneticValue(RandomVariable, distribution_GeneticValue):
+    def __init__(self, *args, **kwargs):
+        RandomVariable.__init__(self, *args, **kwargs)
--- nwas/tests/test_model.py
+++ nwas/tests/test_model.py
@@ -1,9 +1,10 @@
+import edward as ed
 import numpy
 import nwas
 import pytest
 import tensorflow as tf
 
-from nwas.model import GeneticValue, SpikeSlab
+from nwas.model import *
 
 @pytest.fixture
 def tf_session():
@@ -14,14 +15,14 @@ def tf_session():
 def test_spikeslab():
     p = 100
     theta = SpikeSlab(alpha=tf.ones([p, 1]),
-                                 beta=tf.zeros([p, 1]),
-                                 gamma=tf.ones([p, 1]))
+                      beta=tf.zeros([p, 1]),
+                      gamma=tf.ones([p, 1]))
 
 def test_spikeslab_zero_mean(tf_session):
     p = 100
     theta = SpikeSlab(alpha=tf.ones([p, 1]),
-                                 beta=tf.zeros([p, 1]),
-                                 gamma=tf.ones([p, 1]))
+                      beta=tf.zeros([p, 1]),
+                      gamma=tf.ones([p, 1]))
     mean = tf_session.run(theta.mean())
     assert mean.shape == (p, 1)
     assert numpy.isclose(mean, 0).all()
@@ -31,8 +32,8 @@ def test_spikeslab_nonzero_mean(tf_session):
     for _ in range(50):
         beta = numpy.random.normal(size=p).astype('float32')
         theta = SpikeSlab(alpha=tf.ones([p, 1]),
-                                     beta=tf.convert_to_tensor(beta),
-                                     gamma=tf.ones([p, 1]))
+                          beta=tf.convert_to_tensor(beta),
+                          gamma=tf.ones([p, 1]))
         mean = tf_session.run(theta.mean())
         assert numpy.isclose(mean, beta).all()
 
@@ -43,8 +44,8 @@ def test_spikeslab_variance(tf_session):
         beta = tf.zeros([p, 1])
         gamma = tf.ones([p, 1])
         theta = SpikeSlab(alpha=alpha,
-                                     beta=beta,
-                                     gamma=gamma)
+                          beta=beta,
+                          gamma=gamma)
         var = tf_session.run(theta.variance())
         assert var.shape == (p, 1)
         assert numpy.isclose(var, 0.5).all()
@@ -52,14 +53,14 @@ def test_spikeslab_variance(tf_session):
 def test_spikeslab_scalar_hyperparams():
     p = 100
     theta = SpikeSlab(alpha=tf.sigmoid(tf.zeros([1])),
-                                 beta=tf.zeros([p, 1]),
-                                 gamma=tf.nn.softplus(tf.zeros([1])))
+                      beta=tf.zeros([p, 1]),
+                      gamma=tf.nn.softplus(tf.zeros([1])))
 
 def test_spikeslab_zero_mean_scalar_hyperparams(tf_session):
     p = 100
     theta = SpikeSlab(alpha=tf.constant(0.1),
-                                 beta=tf.zeros([p, 1]),
-                                 gamma=tf.ones([1]))
+                      beta=tf.zeros([p, 1]),
+                      gamma=tf.ones([1]))
     mean = tf_session.run(theta.mean())
     assert mean.shape == (p, 1)
     assert numpy.isclose(mean, 0).all()
@@ -69,8 +70,8 @@ def test_spikeslab_nonzero_mean_scalar_hyperparams(tf_session):
     for _ in range(50):
         beta = numpy.random.normal(size=p).astype('float32').reshape(-1, 1)
         theta = SpikeSlab(alpha=tf.constant(0.1),
-                                     beta=tf.convert_to_tensor(beta),
-                                     gamma=tf.ones([1]))
+                          beta=tf.convert_to_tensor(beta),
+                          gamma=tf.ones([1]))
         mean = tf_session.run(theta.mean())
         assert mean.shape == (p, 1)
         assert numpy.isclose(mean, 0.1 * beta).all()
@@ -80,8 +81,8 @@ def test_spikeslab_var_scalar_hyperparams(tf_session):
     for _ in range(50):
         beta = numpy.random.normal(size=p).astype('float32').reshape(-1, 1)
         theta = SpikeSlab(alpha=tf.constant(0.1),
-                                     beta=tf.convert_to_tensor(beta),
-                                     gamma=tf.ones([1]))
+                          beta=tf.convert_to_tensor(beta),
+                          gamma=tf.ones([1]))
         var = tf_session.run(theta.variance())
         assert var.shape == (p, 1)
         assert numpy.isclose(var, .1 + .09 * beta * beta).all()
@@ -90,11 +91,11 @@ def test_spikeslab_kl(tf_session):
     p = 100
     beta = numpy.random.normal(size=p).astype('float32').reshape(-1, 1)
     p_theta = SpikeSlab(alpha=tf.constant(0.1),
-                                   beta=tf.zeros([1]),
-                                   gamma=tf.ones([1]))
+                        beta=tf.zeros([1]),
+                        gamma=tf.ones([1]))
     q_theta = SpikeSlab(alpha=tf.ones([p, 1]),
-                                   beta=tf.convert_to_tensor(beta),
-                                   gamma=tf.ones([p, 1]))
+                        beta=tf.convert_to_tensor(beta),
+                        gamma=tf.ones([p, 1]))
     tf_session.run(tf.contrib.distributions.kl_divergence(q_theta, p_theta))
 
 def test_spikeslab_variable():
@@ -104,6 +105,12 @@ def test_spikeslab_variable():
         beta=tf.Variable(tf.random_normal([p, 1])),
         gamma=tf.nn.softplus(tf.Variable(tf.zeros([p, 1])))
     )
+
+def test_spikeslab_edward_copy():
+    p_theta = SpikeSlab(alpha=tf.constant(0.1),
+                        beta=tf.zeros([1]),
+                        gamma=tf.ones([1]))
+    copy = ed.util.copy(p_theta)
 
 def test_geneticvalue():
     n = 500
@@ -125,7 +132,7 @@ def test_geneticvalue_variable():
         alpha=tf.sigmoid(tf.Variable(tf.zeros([p, m]))),
         beta=tf.Variable(tf.zeros([p, m])),
         gamma=tf.nn.softplus(tf.Variable(tf.zeros([p, m]))))
-    q_eta_ref_m = GeneticValue(x=x_ref, theta=q_theta)
+    GeneticValue(x=x_ref, theta=q_theta)
 
 def test_geneticvalue_shape():
     n = 500
@@ -138,3 +145,26 @@ def test_geneticvalue_shape():
         gamma=tf.nn.softplus(tf.Variable(tf.zeros([p, m]))))
     q_eta_ref_m = GeneticValue(x=x_ref, theta=q_theta)
     assert q_eta_ref_m.shape == (n, m)
+
+def test_geneticvalue_edward_impl():
+    n = 500
+    p = 100
+    m = 10
+    x_ref = numpy.zeros((n, p)).astype('float32')
+    q_theta = SpikeSlab(
+        alpha=tf.sigmoid(tf.Variable(tf.zeros([p, m]))),
+        beta=tf.Variable(tf.zeros([p, m])),
+        gamma=tf.nn.softplus(tf.Variable(tf.zeros([p, m]))))
+    q_eta_ref_m = GeneticValue(x=x_ref, theta=q_theta)
+
+def test_geneticvalue_edward_copy():
+    n = 500
+    p = 100
+    m = 10
+    x_ref = numpy.zeros((n, p)).astype('float32')
+    q_theta = SpikeSlab(
+        alpha=tf.sigmoid(tf.Variable(tf.zeros([p, m]))),
+        beta=tf.Variable(tf.zeros([p, m])),
+        gamma=tf.nn.softplus(tf.Variable(tf.zeros([p, m]))))
+    q_eta_ref_m = GeneticValue(x=x_ref, theta=q_theta)
+    copy = ed.util.copy(q_eta_ref_m)
