2990aaa482c20709950dcc425ea08be33302d90f
Abhishek Sarkar <aksarkar@alum.mit.edu>
2017-11-14 15:44:41 -0600

    Refactor SGVB sampling

--- nwas/sgvb.py
+++ nwas/sgvb.py
@@ -9,13 +9,16 @@ import tensorflow as tf
 def normal_llik(y, mean, prec):
   return -.5 * (-tf.log(prec) + tf.square(y - mean) * prec)
 
-def kl_bernoulli_bernoulli(p, q, reduce=True):
-  """Rasmussen & Williams eq. A.22"""
-  return (p * tf.log(p / q) + (1 - p) * tf.log((1 - p) / (1 - q)))
+def normal_sample(mean, prec, n=1):
+  return mean + tf.random_normal([n, 1]) * tf.sqrt(tf.reciprocal(prec))
 
 def kl_normal_normal(mean_a, prec_a, mean_b, prec_b, reduce=True):
   """Rasmussen & Williams eq. A.23 for univariate Gaussians"""
   return .5 * (1 + tf.log(prec_a) - tf.log(prec_b) + prec_b * (tf.square(mean_a - mean_b) + 1 / prec_a))
+
+def kl_bernoulli_bernoulli(p, q, reduce=True):
+  """Rasmussen & Williams eq. A.22"""
+  return (p * tf.log(p / q) + (1 - p) * tf.log((1 - p) / (1 - q)))
 
 def biased_softplus(x, bias=1e-6):
   return bias + tf.nn.softplus(x)
@@ -29,7 +32,26 @@ def sigmoid(x):
   min_ = np.log(np.finfo('float32').resolution)
   return tf.nn.sigmoid(tf.clip_by_value(x, min_, -min_))
 
-def gaussian_spike_slab(x, y, num_epochs=1000, learning_rate=1e-2, stoch_samples=10, verbose=False):
+def sgvb(feed_dict, error, kl, opt, num_epochs=1000, learning_rate=1e-2, trace=None, verbose=False):
+  elbo = error - tf.add_n(kl)
+  opt.append(elbo)
+  trace_ = [elbo, error] + kl
+  if trace is not None:
+    trace_.extend(trace)
+  optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
+  train = optimizer.minimize(-elbo)
+
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(num_epochs):
+      _, trace_output = sess.run([train, trace_], feed_dict)
+      if np.isnan(trace_output[0]):
+        raise tf.train.NanLossDuringTrainingError
+      if verbose and not i % 100:
+        print(i, *trace_output)
+    return sess.run(opt, feed_dict)
+
+def gaussian_spike_slab(x, y, stoch_samples=10, **kwargs):
   """Fit linear regression with the spike-and-slab prior using SGVB"""
   n, p = x.shape
   graph = tf.Graph()
@@ -57,46 +79,28 @@ def gaussian_spike_slab(x, y, num_epochs=1000, learning_rate=1e-2, stoch_samples
     effect_posterior_var = pip / prec + pip * (1 - pip) * tf.square(mean)
 
     eta_mean = tf.matmul(x_ph, effect_posterior_mean)
-    eta_std = tf.sqrt(tf.matmul(tf.square(x_ph), effect_posterior_var))
-    noise = tf.random_normal([stoch_samples, 4])
-    eta = eta_mean + noise[:,0] * eta_std
-    phi = biased_softplus(effect_prec_mean + noise[:,1] * tf.sqrt(tf.reciprocal(effect_prec_prec)))
-    odds = sigmoid(logodds_mean + noise[:,2] * tf.sqrt(tf.reciprocal(logodds_prec)))
-    effect_prec = tf.exp(effect_prec_mean + noise[:,3] * tf.sqrt(tf.reciprocal(effect_prec_prec)))
+    eta_var = tf.matmul(tf.square(x_ph), effect_posterior_var)
 
-    llik = tf.reduce_mean(tf.reduce_sum(normal_llik(y_ph, eta, phi), axis=0))
-    kl_terms = [
+    eta = normal_sample(eta_mean, tf.reciprocal(eta_var), stoch_samples)
+    resid_prec = biased_softplus(normal_sample(resid_prec_mean, resid_prec_prec, stoch_samples))
+    odds = sigmoid(normal_sample(logodds_mean, logodds_prec, stoch_samples))
+    effect_prec = tf.exp(normal_sample(effect_prec_mean, effect_prec_prec, stoch_samples))
+
+    error = tf.reduce_mean(tf.reduce_sum(normal_llik(y_ph, eta, resid_prec), axis=0))
+    kl = [
       tf.reduce_mean(tf.reduce_sum(kl_bernoulli_bernoulli(pip, odds), axis=0)),
       tf.reduce_mean(tf.reduce_sum(pip * kl_normal_normal(mean, prec, tf.constant(0.), effect_prec), axis=0)),
       tf.reduce_sum(kl_normal_normal(logodds_mean, logodds_prec, tf.constant(0.), tf.constant(1.))),
       tf.reduce_sum(kl_normal_normal(effect_prec_mean, effect_prec_prec, tf.constant(0.), tf.constant(1.))),
     ]
-    elbo = llik - tf.add_n(kl_terms)
 
     # GLM coefficient of determination
     R = 1 - tf.reduce_sum(tf.square(y_ph - eta_mean)) / tf.reduce_sum(tf.square(y_ph - tf.reduce_mean(y_ph)))
-
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
-    train = optimizer.minimize(-elbo)
-    trace = [elbo, llik, R] + kl_terms
     opt = [pip, effect_posterior_mean, effect_posterior_var,
            logodds_mean, tf.reciprocal(biased_softplus(logodds_prec)),
            effect_prec_mean,
-           tf.reciprocal(biased_softplus(effect_prec_prec)), elbo]
-
-    with tf.Session() as sess:
-      sess.run(tf.global_variables_initializer())
-      for i in range(num_epochs):
-        _, trace_output = sess.run([train, trace], feed_dict={x_ph: x, y_ph: y})
-        if np.isnan(trace_output[0]):
-          raise tf.train.NanLossDuringTrainingError
-        if verbose and not i % 100:
-          print(i, *trace_output)
-      return sess.run(opt, feed_dict={x_ph: x, y_ph: y})
-
-def logit(x):
-  min_ = np.log(np.finfo('float32').resolution)
-  return tf.clip_by_value(tf.log(x) - tf.log1p(-x), min_, -min_)
+           tf.reciprocal(biased_softplus(effect_prec_prec))]
+    return sgvb({x_ph: x, y_ph: y}, error, kl, opt, trace=[R], **kwargs)
 
 def project_simplex(x):
   """Project x onto the probability simplex
@@ -116,7 +120,19 @@ def project_simplex(x):
 def kl_categorical_categorical(logits_p, logits_q):
   return tf.nn.sigmoid(logits_p) * (tf.nn.softplus(-logits_q) - tf.nn.softplus(-logits_p))
 
-def gaussian_categorical_slab(x, y, l, num_epochs=1000, stoch_samples=10, learning_rate=1e-2):
+def categorical_slab_cov(probs, mean, prec):
+  probs /= tf.reduce_sum(probs, axis=1, keep_dims=True)
+  if probs.shape[0] == 1:
+    cov = tf.zeros([probs.shape[1], probs.shape[1]])
+  else:
+    cov = (tf.matmul(probs, probs, transpose_a=True) *
+           tf.matmul(mean, mean, transpose_a=True))
+  diag = tf.diag(tf.reduce_sum(probs * (tf.square(mean) + tf.reciprocal(prec)),
+                               axis=0))
+  cov += diag
+  return cov
+
+def gaussian_categorical_slab(x, y, l, stoch_samples=10, **kwargs):
   """Fit the model assuming l causal variants"""
   n, p = x.shape
   graph = tf.Graph()
@@ -137,47 +153,41 @@ def gaussian_categorical_slab(x, y, l, num_epochs=1000, stoch_samples=10, learni
       mu = tf.get_variable('mu', [l, p])
       phi = biased_softplus(tf.get_variable('phi', [l, p]))
 
-    # TODO: priors appear only in the KL terms which is tricky for
-    # reading/understanding, but bayesflow/Edward don't DTRT
-    kl_terms = [
+    b_mean = tf.transpose(tf.reduce_sum(pi * mu, axis=0, keep_dims=True))
+    # Important: there is non-zero covariance, but we're not going to use it
+    # here
+    b_var = tf.transpose(tf.reduce_sum(pi / phi + pi * (1 - pi) * tf.square(mu), axis=0, keep_dims=True))
+
+    # eta = x b'
+    eta_mean = tf.matmul(x, b_mean)
+    # Important: this is not the same as for the spike-and-slab prior because of Cov(b_j, b_k)!
+    # Matrix Cookbook eq. 313
+    eta_cov = tf.matmul(tf.matmul(x, categorical_slab_cov(pi, mu, phi)), x, transpose_b=True)
+    jitter = tf.constant(1e-6) * tf.reduce_mean(tf.diag_part(eta_cov)) * tf.eye(tf.shape(eta_cov)[0])
+    eta_l = tf.cholesky(eta_cov + jitter)
+    # [stoch_samples, n]
+    eta = tf.transpose(eta_mean) + tf.transpose(tf.matmul(eta_l, tf.random_normal([n, stoch_samples])))
+    v = tf.exp(normal_sample(v_mean, v_prec, stoch_samples))
+    error = tf.reduce_mean(tf.reduce_sum(normal_llik(y_ph, eta, v), axis=1))
+
+    v_b = tf.exp(normal_sample(v_b_mean, v_b_prec, stoch_samples))
+    kl = [
       tf.reduce_sum(kl_normal_normal(v_mean, v_prec, tf.constant(0.), tf.constant(1.))),
       tf.reduce_sum(kl_normal_normal(v_b_mean, v_b_prec, tf.constant(0.), tf.constant(1.))),
       tf.reduce_sum(kl_categorical_categorical(logit_pi, tf.fill([l, p], -tf.log(p - 1.)))),
-      tf.reduce_sum(kl_normal_normal(mu, phi, tf.constant(0.), biased_softplus(v_b_mean))),
+      tf.reduce_mean(tf.reduce_sum(kl_normal_normal(mu, phi, tf.constant(0.), biased_softplus(v_b_mean)), axis=1)),
     ]
-
-    b_mean = tf.transpose(tf.reduce_sum(pi * mu, axis=0, keep_dims=True))
-    b_var = tf.transpose(tf.reduce_sum(pi / phi + pi * (1 - pi) * tf.square(mu), axis=0, keep_dims=True))
-    eta_mean = tf.matmul(x, b_mean)
-    eta_var = tf.matmul(tf.square(x), b_var)
-
-    noise = tf.random_normal([stoch_samples, 2])
-    eta_samples = eta_mean + noise[:,0] * tf.sqrt(eta_var)
-    v_samples = biased_softplus(v_mean + noise[:,1] * tf.sqrt(tf.reciprocal(v_prec)))
-
-    llik = tf.reduce_mean(tf.reduce_sum(normal_llik(y_ph, eta_samples, v_samples), axis=1))
-    elbo = llik - tf.add_n(kl_terms)
-
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
-    train = optimizer.minimize(-elbo)
-
-    with tf.control_dependencies([train]):
-      pi = project_simplex(pi)
-      train = tf.assign(logit_pi, tf.log(pi) - tf.log1p(-pi))
 
     # GLM coefficient of determination
     R = 1 - tf.reduce_sum(tf.square(y_ph - eta_mean)) / tf.reduce_sum(tf.square(y_ph - tf.reduce_mean(y_ph)))
-    trace = [elbo, llik, R] + kl_terms
-    opt = [pi, mu, phi, biased_softplus(v_mean), v_prec, biased_softplus(v_b_mean, bias=1e-3), v_b_prec]
+    opt = [pi, b_mean, b_var, v_mean, v_prec, v_b_mean, v_b_prec]
 
-  with tf.Session(graph=graph) as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(num_epochs):
-      _, trace_output = sess.run([train, trace], feed_dict={x_ph: x, y_ph: y})
-      if np.isnan(trace_output).any():
-        raise tf.train.NanLossDuringTrainingError
-      if (np.array(trace_output[-4:]) <= 0).any():
-        import pdb; pdb.set_trace()
-      if not i % 100:
-        print(i, *trace_output)
-    return sess.run(opt, feed_dict={x_ph: x, y_ph: y})
+    return sgvb({x_ph: x, y_ph: y}, error, kl, opt, trace=[R], **kwargs)
+
+if __name__ == '__main__':
+  import nwas
+  
+  with nwas.simulation.simulation(p=1000, pve=0.5, annotation_params=[(10, 1)], seed=0) as s:
+    x, y = s.sample_gaussian(n=500)
+    x = x.astype('float32')
+    y = y.reshape(-1, 1).astype('float32')
